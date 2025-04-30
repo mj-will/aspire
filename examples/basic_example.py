ffe575603f70a43492ae642c095370fb69b5b30d
mj-will <michaeljw1@googlemail.com>
2025-04-30 11:22:07 +0100

    EXAMP: update basic example

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -4,31 +4,35 @@ likelihood with a uniform prior.
 """
 
 import math
+from pathlib import Path
 
-import torch
-import torch.distributions
+import numpy as np
+from scipy.stats import norm, uniform
 
 from poppy import Poppy
 from poppy.samples import Samples
-from poppy.utils import configure_logger
+from poppy.utils import configure_logger, PoppyFile
+from poppy.plot import plot_comparison
 
 # Configure the logger
 configure_logger("INFO")
 
-# Number of dimensions
-dims = 16
+outdir = Path("outdir") / "basic_example"
+outdir.mkdir(parents=True, exist_ok=True)
 
+# Number of dimensions
+dims = 4
 
 # Define the log likelihood and log prior
-def log_likelihood(samples: Samples) -> torch.Tensor:
+def log_likelihood(samples: Samples):
     # The log likelihood must accept a Samples object
     # The samples object contains the samples in the attribute samples.x
-    return torch.distributions.Normal(2, 1).log_prob(samples.x).sum(axis=-1)
+    return norm(2, 1).logpdf(samples.x).sum(axis=-1)
 
 
-def log_prior(samples: Samples) -> torch.Tensor:
+def log_prior(samples: Samples):
     return (
-        torch.distributions.Uniform(-10, 10).log_prob(samples.x).sum(axis=-1)
+        uniform(-10, 20).logpdf(samples.x).sum(axis=-1)
     )
 
 
@@ -36,7 +40,8 @@ def log_prior(samples: Samples) -> torch.Tensor:
 true_log_evidence = -dims * math.log(20)
 
 # Generate some initial samples
-initial_samples = Samples(1.0 * torch.randn(5000, dims) + 2.0)
+# These are slightly biased compared to the true posterior
+initial_samples = Samples(norm(2.5, 1.0).rvs(size=(5000, dims)))
 # Define the parameters and prior bounds
 parameters = [f"x_{i}" for i in range(dims)]
 prior_bounds = {p: [-10, 10] for p in parameters}
@@ -48,39 +53,38 @@ poppy = Poppy(
     dims=dims,
     parameters=parameters,
     prior_bounds=prior_bounds,
-    flow_matching=False,
-    flow_backend="zuko",
 )
 
 # Fit the flow to the initial samples
 history = poppy.fit(
     initial_samples,
     n_epochs=50,
-    lr_annealing=True,
 )
 # Plot the loss
 fig = history.plot_loss()
-fig.savefig("loss.png")
+fig.savefig(outdir / "loss.png")
 
 # Produce samples from the posterior
-samples = poppy.sample_posterior(2000)
+samples = poppy.sample_posterior(5000)
 
-# Produce a corner plot showing the samples
-corner_kwargs = dict(
-    density=True,
-    bins=30,
-    color="C0",
-    hist_kwargs=dict(density=True, color="C0"),
+# Save the the results to a file
+# The PoppyFile is a small wrapper around h5py.File that automatically
+# includes additional metadata
+with PoppyFile(outdir / "poppy_result.h5", "w") as f:
+    poppy.save_config(f, "poppy_config")
+    samples.save(f, "posterior_samples")
+    history.save(f, "flow_history")
+
+fig = plot_comparison(
+    initial_samples,
+    samples,
+    samples,
+    per_samples_kwargs=[
+        dict(include_weights=True, color="C0"),
+        dict(include_weights=False, color="lightgrey"),
+        dict(include_weights=True, color="C1"),
+    ],
+    labels=["Training samples", "Samples (w/o weights)", "Posterior samples"],
 )
-fig = None
-# Plot the initial samples
-fig = poppy.training_samples.plot_corner(**corner_kwargs)
-# Plot the samples without the weights
-corner_kwargs["color"] = "lightgrey"
-corner_kwargs["hist_kwargs"]["color"] = "lightgrey"
-fig = samples.plot_corner(fig=fig, include_weights=False, **corner_kwargs)
-# Plot the samples with the weights
-corner_kwargs["color"] = "C1"
-corner_kwargs["hist_kwargs"]["color"] = "C1"
-fig = samples.plot_corner(fig=fig, **corner_kwargs)
-fig.savefig("comparison.png")
+
+fig.savefig(outdir / "comparison.png")
