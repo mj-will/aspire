9b4159b91b708ef7e788e8f8e8bb69d423378d91
mj-will <michaeljw1@googlemail.com>
2025-04-30 11:21:57 +0100

    MAINT: change logging format

--- src/poppy/utils.py
+++ src/poppy/utils.py
@@ -32,7 +32,7 @@ def configure_logger(log_level: str | int = "INFO") -> logging.Logger:
     ch = logging.StreamHandler()
     ch.setLevel(log_level)
     formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        "%(asctime)s - poppy - %(levelname)s - %(message)s"
     )
     ch.setFormatter(formatter)
     logger.addHandler(ch)
