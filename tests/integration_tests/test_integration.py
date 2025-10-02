cc247193cbc1ecc464a7d7d7c4559901dfda4d32
jacopok <jacopok@gmail.com>
2025-10-02 10:21:25 +0200

    STY: apply ruff linter to test

--- tests/integration_tests/test_integration.py
+++ tests/integration_tests/test_integration.py
@@ -68,6 +68,7 @@ def test_integration_flowjax(
         **sampler_config.sampler_kwargs,
     )
 
+
 def test_init_existing_flow(
     log_likelihood,
     log_prior,
@@ -78,26 +79,25 @@ def test_init_existing_flow(
     bounded_to_unbounded,
     sampler_config,
 ):
-
     aspire_kwargs = {
-        'log_likelihood': log_likelihood,
-        'log_prior': log_prior,
-        'dims': dims,
-        'parameters': parameters,
-        'prior_bounds': prior_bounds,
-        'flow_matching': False,
-        'bounded_to_unbounded': bounded_to_unbounded,
-        'flow_backend': "zuko",
+        "log_likelihood": log_likelihood,
+        "log_prior": log_prior,
+        "dims": dims,
+        "parameters": parameters,
+        "prior_bounds": prior_bounds,
+        "flow_matching": False,
+        "bounded_to_unbounded": bounded_to_unbounded,
+        "flow_backend": "zuko",
     }
 
     aspire = Aspire(**aspire_kwargs)
     aspire.init_flow()
-    
+
     saved_flow = aspire.flow
-    new_aspire_obj = Aspire(**aspire_kwargs | {'flow': saved_flow})
+    new_aspire_obj = Aspire(**aspire_kwargs | {"flow": saved_flow})
 
     assert new_aspire_obj.flow is aspire.flow
-    
+
     new_aspire_obj.flow = saved_flow
-    
+
     assert new_aspire_obj.flow is saved_flow
