80b495095a38a4ea8d8605daa399acba3054c06c
Michael J. Williams <michaeljw1@googlemail.com>
2026-02-11 15:55:39 +0000

    ENH: add function to enable scipy array api

--- src/aspire/__init__.py
+++ src/aspire/__init__.py
@@ -7,6 +7,7 @@ from importlib.metadata import PackageNotFoundError, version
 
 from .aspire import Aspire
 from .samples import Samples
+from .utils import enable_scipy_array_api
 
 try:
     __version__ = version("aspire")
@@ -14,6 +15,8 @@ except PackageNotFoundError:
     __version__ = "unknown"
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+enable_scipy_array_api()
 
 __all__ = [
     "Aspire",
