3ecbcc56b17c7d488922b521db325c05f696d329
Michael J. Williams <michaeljw1@googlemail.com>
2026-05-22 12:29:28 +0100

    BUG: ensure history contains float and numpy arrays

--- src/aspire/samplers/smc/base.py
+++ src/aspire/samplers/smc/base.py
@@ -378,11 +378,11 @@ class SMCSampler(MCMCSampler):
                     beta_tolerance=beta_tolerance,
                 )
                 self.history.eff_target.append(
-                    self.current_target_efficiency(beta)
+                    float(self.current_target_efficiency(beta))
                 )
 
                 logger.info(f"it {iterations} - beta: {beta}")
-                self.history.beta.append(beta)
+                self.history.beta.append(float(beta))
 
                 ess = effective_sample_size(samples.log_weights(beta))
                 eff = ess / len(samples)
@@ -390,20 +390,22 @@ class SMCSampler(MCMCSampler):
                     logger.warning(
                         f"it {iterations} - Low sample efficiency: {eff:.2f}"
                     )
-                self.history.ess.append(ess)
+                self.history.ess.append(float(ess))
                 logger.info(
                     f"it {iterations} - ESS: {ess:.1f} ({eff:.2f} efficiency)"
                 )
                 self.history.ess_target.append(
-                    effective_sample_size(samples.log_weights(1.0))
+                    float(effective_sample_size(samples.log_weights(1.0)))
                 )
 
                 log_evidence_ratio = samples.log_evidence_ratio(beta)
                 log_evidence_ratio_var = samples.log_evidence_ratio_variance(
                     beta
                 )
-                self.history.log_norm_ratio.append(log_evidence_ratio)
-                self.history.log_norm_ratio_var.append(log_evidence_ratio_var)
+                self.history.log_norm_ratio.append(float(log_evidence_ratio))
+                self.history.log_norm_ratio_var.append(
+                    float(log_evidence_ratio_var)
+                )
                 logger.info(
                     f"it {iterations} - Log evidence ratio: {log_evidence_ratio:.2f} +/- {np.sqrt(log_evidence_ratio_var):.2f}"
                 )
@@ -412,7 +414,7 @@ class SMCSampler(MCMCSampler):
 
                 samples = self.mutate(samples, beta)
                 if store_sample_history:
-                    self.history.sample_history.append(samples)
+                    self.history.sample_history.append(samples.to_numpy())
                 maybe_checkpoint()
                 if beta == 1.0 or (
                     max_n_steps is not None and iterations >= max_n_steps
